4226ad2320d4220e9b80a03866b6b4d27d8d8518
Karan <karan@akinariwala.com>
2020-12-11 18:30:49 +0530

    Remove language parameter from API parameters as en-us is default

--- weather.py
+++ weather.py
@@ -3,12 +3,10 @@ import time
 from decouple import config
 
 def get_forecast(secret_key, location):
-    language = "en-us"
     url_for_location_key = "http://dataservice.accuweather.com/locations/v1/cities/search"
     params = {
         "apikey": secret_key,
         "q": location,
-        "language": language,
     }
     request_for_loc_key = requests.get(url = url_for_location_key, params = params)
     print(request_for_loc_key)
@@ -16,7 +14,6 @@ def get_forecast(secret_key, location):
     url_for_1hour_forecast = "http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/" + locationKey
     params_forecast = {
         "apikey": secret_key,
-        "language": language,
         "metric": True
     }
     request_for_forecast = requests.get(url = url_for_1hour_forecast, params = params_forecast)
