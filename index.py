56eadc158d29bf792570d858821fd0d5ac4ddfc4
Karan <karan@akinariwala.com>
2020-12-11 18:40:40 +0530

    Imported the API Call function into the Flask App

--- index.py
+++ index.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from weather import get_forecast
+from decouple import config
+
+secret_key = config("api_key")
+
+location = "Bengaluru"
+#forecast_data = get_forecast(secret_key, location)
+#date_forecast = forecast_data["DateTime"].split("T")[0]
+#time_forecast = forecast_data["DateTime"].split("T")[1]
+#temperature = forecast_data["Temperature"]["Value"]
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return location
